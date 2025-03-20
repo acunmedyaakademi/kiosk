ca90150519ca882cb2e048246de2bdad343cfb11
“Gökdeniz <gokdenizkeeles@gmail.com>
2025-03-20 19:05:17 +0300

    header iskelet olusturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './styles/App.css'
 import "./styles/reset.css"
+import Header from './components/Header';
+
+import { supabase } from "./supabase";
 
 function App() {
   const [count, setCount] = useState(0);
 
+
+
+
   return (
     <>
-      <h2>deneme</h2>
+      <Header />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
+import "../styles/Header.css"
+
 export default function Header() {
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getDate()} ${currentDate.toLocaleString('default', { month: 'long' })} ${currentDate.getFullYear()}`;
+
   return (
     <header>
-      <h1>TostCum</h1>
-      <i class="fa-solid fa-cart-shopping" style={{ color: "#0c0c0d", }}></i>
+      <div className="flex">
+        <h1>Tost/Cum</h1>
+        <span>{formattedDate}</span>
+      </div>
+
+      <div>
+        <i className="fa-solid fa-globe">TR</i>
+        <select name="" id="" >
+          <option value="">eng</option>
+        </select>
+      </div>
+
     </header>
   )
 }
