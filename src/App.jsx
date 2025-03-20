1779f5f4b5a2ae19f340752f2be844ca7fee3861
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2025-03-20 15:47:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import "./reset.css"
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <h2>asdasd</h2>
+    </>
+  )
+}
+
+export default App
