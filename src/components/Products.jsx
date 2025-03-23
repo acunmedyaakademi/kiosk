11e01f78a5b719118740ec780d3011ec984d4176
“Gökdeniz <gokdenizkeeles@gmail.com>
2025-03-23 21:15:19 +0300

    order taslagı eklendi

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import supabase from "../js/supabaseClient"
 import "../styles/Products.css"
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState([]);
+  const dialogRef = useRef(null);
 
   useEffect(() => {
     async function getData() {
@@ -42,25 +43,44 @@ export default function Products() {
 
   }
 
+  function openDialog() {
+    dialogRef.current.show();
+  }
+
 
 
   return (
     <>
-    <h2>All Items</h2>
-    <div className="products-area">
-      {products.map(x => (
-        <div className="product">
-          <img src={x.img} />
-          <div>
-            <h6>{x.name}</h6>
-            <span>{x.price} ₺</span>
+      <h2>All Items</h2>
+      <div className="products-area">
+        {products.map(x => (
+          <div className="product">
+            <img src={x.img} />
             <div>
-              <button onClick={() => handleProduct(x)}>+ Ekle</button>
+              <h6>{x.name}</h6>
+              <span>{x.price} ₺</span>
+              <div>
+                <button onClick={() => { handleProduct(x); openDialog(); }}>+ Ekle</button>
+              </div>
             </div>
           </div>
+        ))}
+      </div>
+      <dialog ref={dialogRef} className="order-dialog">
+        <div className="relative">
+          <h2>Selected Items</h2>
+          {selectedProduct.map(x => (
+            <>
+              <img src={x.img} alt="" className="auto" />
+              <div className="order-sidebar">
+                <img src={x.img} alt="" />
+                <p>{x.name} - {x.count} adet</p>
+                <span>{x.price * x.count} ₺</span>
+              </div>
+            </>
+          ))}
         </div>
-      ))}
-    </div>
-      </>
+      </dialog>
+    </>
   )
 }
